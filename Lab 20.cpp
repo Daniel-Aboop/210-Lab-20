5d2d9a54fc62e6413c1f472a6c13068b8f22f6b9
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-10-05 17:14:34 -0700

    5:14 PM

--- Lab 20.cpp
+++ Lab 20.cpp
@@ -64,20 +64,18 @@ int main() {
     chairPtr->print();
 
     //creating dynamic chair object with constructor
-    cout<<"DYNMAIC CHAIR OBKECT"<<endl;
+    // did the same thing and removed the setprices function so to leave it as random everytime.
     Chair *livingChair = new Chair();
     livingChair->print();
     delete livingChair;
     livingChair = nullptr;
 
     //creating dynamic array of chair objects
+    // just removed the set prices functions from here
     Chair *collection = new Chair[SIZE];
     collection[0].setLegs(4);
-    collection[0].setPrices(441.41, 552.52, 663.63);
     collection[1].setLegs(4);
-    collection[1].setPrices(484.84, 959.59, 868.68);
     collection[2].setLegs(4);
-    collection[2].setPrices(626.26, 515.15, 757.57);
     for (int i = 0; i < SIZE; i++)
         collection[i].print();
     
